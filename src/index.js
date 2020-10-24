97116a0ba3a805a8a49410b0bc6b8355d5274d9d
Andrei Mitskevich <metro.trend.now@gmail.com>
2020-10-24 14:47:54 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
+
+
+    modul = Math.abs(n);
+   
+  // приведеник к стринг здесь нужно?
+  
+    let  strii = String(modul)
+
+    strNumb=strii.split("");
+
+    reverseNumb=strNumb.reverse();
+ 
+    joinNumb=reverseNumb.join('');
+  
+    return joinNumb;
+
   
 }
